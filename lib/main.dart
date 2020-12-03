6578b83bddbb326e57fdbbe415e69a2af7dd097c
Srilal Sachintha <SrilalSachintha@Gmail.com>
2020-12-03 19:05:44 +0530

    ccx

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mad_project/UI/Home.dart';
 
 void main() {
   runApp(ClubsPlus());
@@ -12,7 +13,7 @@ class ClubsPlus extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
