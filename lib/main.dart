54c9b5fb17fd1b71ba0425a7d2da65fa03b52d9d
Srilal Sachintha <SrilalSachintha@Gmail.com>
2020-12-03 19:43:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mad_project/UI/Home.dart';
 
 void main() {
@@ -8,7 +9,7 @@ void main() {
 class ClubsPlus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Clubs Plus',
       theme: ThemeData(
         primarySwatch: Colors.blue,
