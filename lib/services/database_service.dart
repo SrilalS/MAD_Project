5841fdc83aca2c2550e06bb3ceab2c690f567962
dehemiweerakkody <68898805+dehemiweerakkody@users.noreply.github.com>
2020-12-08 19:53:23 +0530

    event screens

---
+++ lib/services/database_service.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mad_project/models/post_model.dart';
+
+class DatabaseService {
+  FirebaseFirestore _db = FirebaseFirestore.instance;
+//   FirebaseFirestore reference = FirebaseFirestore.instance;
+
+  Future<void> setPost(Post post) {
+    var options = SetOptions(merge: true);
+    return _db.collection('Posts').doc(post.caption).set(post.toMap(), options);
+  }
+
+  Future<void> deletePost(String postId) {
+    return _db.collection('post').doc(postId).delete();
+  }
+
+  Stream<List<Post>> getPostfromPostCollection() {
+    return _db.collection('Posts').snapshots().map((snapshot) =>
+        snapshot.docs.map((doc) => Post.fromJson(doc.data())).toList());
+  }
+
+  Stream<List<Post>> getPostfromclubName(String clubName) {
+    return _db
+        .collection('Posts')
+        .where('clubName', isEqualTo: clubName)
+        .snapshots()
+        .map((snapshot) =>
+            snapshot.docs.map((doc) => Post.fromJson(doc.data())).toList());
+  }
+
+  Stream<List<Post>> getEventfromclubName(String clubName) {
+    return _db
+        .collection('Posts')
+        .where('clubName', isEqualTo: clubName)
+        .snapshots()
+        .map((snapshot) =>
+            snapshot.docs.map((doc) => Post.fromJson(doc.data())).toList());
+  }
+}
--- lib/widgets/event_container.dart
+++ lib/widgets/event_container.dart
@@ -1,12 +1,16 @@
-import 'package:mad_project/models/models.dart';
 
 import 'package:mad_project/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
 
 class EventContainer extends StatelessWidget {
-  final Post post;
+  final String caption;
+  final String imageUrl;
+  final String clubImage;
+  final String clubName;
 
-  const EventContainer({Key key, this.post}) : super(key: key);
+  const EventContainer({Key key, this.caption, this.imageUrl, this.clubImage, this.clubName}) : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,64 +24,69 @@ class EventContainer extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  _postHeader(post: post),
-                  const SizedBox(
-                    height: 4.0,
-                  ),
-                  Text(post.caption),
-                  post.imageUrl != null
-                      ? const SizedBox.shrink()
-                      : const SizedBox(
-                          height: 6.0,
-                        )
+                  _postHeader(clubImage: clubImage,clubName: clubName,),
+                  const SizedBox(height: 4.0,),
+                  Text(caption),
+                  imageUrl != null ? const SizedBox.shrink() : const SizedBox(height: 6.0,)
+
                 ],
               ),
             ),
-            post.imageUrl != null
-                ? Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Image.network(post.imageUrl),
-                    // CachedNetworkImage(imageUrl: post.imageUrl),
-                  )
-                : const SizedBox.shrink(),
+            imageUrl != null ?
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: _postStats(post: post),
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Image.network(imageUrl),
+              // CachedNetworkImage(imageUrl: post.imageUrl),
+            )
+
+                : const SizedBox.shrink(),
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: _postStats(clubName: clubName,),
             )
           ],
-        ));
+        )
+
+
+    );
   }
 }
 
-class _postHeader extends StatelessWidget {
-  final Post post;
+class _postHeader  extends StatelessWidget {
+  final String clubImage;
+  final String clubName;
 
-  const _postHeader({Key key, @required this.post}) : super(key: key);
+  const _postHeader({Key key, this.clubImage, this.clubName}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        ProfileAvatar(imageUrl: post.user.imageUrl),
-        const SizedBox(width: 8.0),
+        ProfileAvatar(imageUrl: clubImage),
+        const SizedBox(width:8.0),
         Expanded(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              post.user.name,
-              style: const TextStyle(fontWeight: FontWeight.w600),
-            ),
-            Row(
+            child:   Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(
-                  '${post.timeAgo} .',
-                  style: TextStyle(color: Colors.grey[600], fontSize: 12.0),
+                Text(clubName,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w600
+                  ),
                 ),
-                Icon(Icons.public, color: Colors.grey[600], size: 12.0)
+                Row(
+                  children: <Widget>[
+//                    Text('${post.timeAgo} .',
+//                      style:  TextStyle(color: Colors.grey[600],
+//                          fontSize: 12.0
+//                      ),
+//                    ),
+                    Icon(Icons.public,
+                        color: Colors.grey[600],
+                        size: 12.0
+                    )
+                  ],
+                )
               ],
             )
-          ],
-        )),
+        ),
         // IconButton(icon: const Icon(Icons.more_horiz),
         //  onPressed: () => print('More'),
         //  )
@@ -87,9 +96,9 @@ class _postHeader extends StatelessWidget {
 }
 
 class _postStats extends StatelessWidget {
-  final Post post;
+final String clubName;
 
-  const _postStats({Key key, @required this.post}) : super(key: key);
+  const _postStats({Key key, this.clubName}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -98,25 +107,25 @@ class _postStats extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Container(
+
               width: 100,
               height: 25,
               padding: const EdgeInsets.all(4.0),
               child: RaisedButton(
                 color: Colors.blue[400],
                 onPressed: () {
-                  //        Navigator.push(context,
-                  //     MaterialPageRoute(
-                  // builder:(context) => ClubPage()
-                  //  ),);
+                  // Navigator.push(context,
+                  //   MaterialPageRoute(
+                  //       builder:(context) => ClubPage(clubName: clubName,)
+                  //   ),);
                 },
                 child: const Text('View Club',
-                    style: TextStyle(fontSize: 11, color: Colors.white)),
+                    style: TextStyle(fontSize: 11 , color: Colors.white)),
               ),
             ),
-            const SizedBox(
-              width: 4.0,
-            ),
+            const SizedBox(width: 4.0,),
             Container(
+
               width: 140,
               height: 25,
               padding: const EdgeInsets.all(4.0),
@@ -126,13 +135,16 @@ class _postStats extends StatelessWidget {
                   print("Register");
                 },
                 child: const Text('Register Event',
-                    style: TextStyle(fontSize: 11, color: Colors.white)),
+                    style: TextStyle(fontSize: 11 , color: Colors.white)),
               ),
             ),
+
           ],
         ),
         const Divider(),
+
       ],
     );
+
   }
 }
