6efbe573507867951ce4b71c80179ffb7833b0a6
Srilal Sachintha <SrilalSachintha@Gmail.com>
2020-12-12 16:38:09 +0530

    Update Send.dart

--- lib/UI/Admin/Notifications/Send.dart
+++ lib/UI/Admin/Notifications/Send.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +10,20 @@ class SendNotifi extends StatefulWidget {
 
 class _SendNotifiState extends State<SendNotifi> {
 
-  void sendNotification(String title, String content){
+  void sendNotification(String title, String content) async{
+    var dataJSON = {
+      "app_id": "36ac3004-610a-4d04-9f6a-e65ba0e7c5f5",
+      "included_segments": ["All"],
+      "data": {"foo": "bar"},
+      "contents": {"en": content},
+      "title": {"en": title}
+    };
+
     Dio dio = new Dio();
-    dio.options.baseUrl = 'https://onesignal.com/api/v1/notifications';
+    //dio.options.baseUrl = 'https://onesignal.com/api/v1/notifications';
     dio.options.headers['content-Type'] = 'application/json';
     dio.options.headers["authorization"] = "Basic YjYwNWFhNGMtNGQxMi00YjNiLWJhNWUtMzE4NWI5ZjcyY2Q3";
-    dio
+    await dio.post('https://onesignal.com/api/v1/notifications', data: jsonEncode(dataJSON));
   }
 
   @override
@@ -22,7 +32,11 @@ class _SendNotifiState extends State<SendNotifi> {
       body: Center(
         child: Column(
           children: [
-
+            RaisedButton(
+              onPressed: (){
+                  sendNotification('title', 'content');
+              },
+            )
           ],
         ),
       ),
