5bd08e3fe03919e383286b8b89209dae2d797ad2
Srilal Sachintha <SrilalSachintha@Gmail.com>
2020-12-12 15:26:36 +0530

    xcx

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,25 @@ import 'package:get/get.dart';
 import 'package:mad_project/UI/Auth.dart';
 import 'package:mad_project/UI/FirebaseListing.dart';
 import 'package:mad_project/UI/Home.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ClubsPlus());
 }
 
 class ClubsPlus extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    Firebase.initializeApp();
+    OneSignal.shared.setLogLevel(OSLogLevel.none, OSLogLevel.none);
+    OneSignal.shared.init(
+        "36ac3004-610a-4d04-9f6a-e65ba0e7c5f5",
+    );
+
+    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+
     return GetMaterialApp(
       title: 'Clubs Plus',
       debugShowCheckedModeBanner: false,
