818c46d8097bced6156252b4fe19a908b7357a5a
Srilal Sachintha <SrilalSachintha@Gmail.com>
2020-12-12 15:28:32 +0530

    Create Send.dart

---
+++ lib/UI/Admin/Notifications/Send.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SendNotifi extends StatefulWidget {
+  @override
+  _SendNotifiState createState() => _SendNotifiState();
+}
+
+class _SendNotifiState extends State<SendNotifi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
