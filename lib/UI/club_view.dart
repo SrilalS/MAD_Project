10b40ea6f9dde35b16ed42730fd3d5143f8f6a89
tharu-98 <64526055+tharu-98@users.noreply.github.com>
2021-01-05 09:09:55 +0530

    Update club_view.dart

--- lib/UI/club_view.dart
+++ lib/UI/club_view.dart
@@ -1,29 +1,105 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Widget> containers = [
+    Container(
+      color: Colors.pink,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.orange,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            // ignore: todo
-            // TODO: Add background here
-            Image.network(post.imageUrl),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 80),
-                CircleAvatar(
-                  radius: 58,
-                  // ignore: todo
-                  // TODO: Add avatar here
-                  backgroundImage: NetworkImage(
-                      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nsbm.ac.lk%2Fenroll-now%2F&psig=AOvVaw3eiX2TAB9ErnLj-0-wGCVd&ust=1609766537261000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLC35-nt_-0CFQAAAAAdAAAAABAJ"),
+      // appBar: null,
+      body: new ListView(
+        children: <Widget>[
+          Container(
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                      height: 150.0,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://www.htmlcsscolor.com/preview/gallery/33DFFF.png'))),
+                    ))
+                  ],
+                ),
+                Positioned(
+                    top: 80.0,
+                    child: Container(
+                        height: 190.0,
+                        width: 190.0,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    'https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png')),
+                            border:
+                                Border.all(color: Colors.black, width: 6.0))))
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            height: 170.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Rotrac Club',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28.0),
+                ),
+                SizedBox(
+                  width: 5.0,
+                ), // Icon(Icons.person, color: Colors.blueAccent,)
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('hello'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  text: '2020',
+                ),
+                Tab(
+                  text: '2021',
+                ),
+                Tab(
+                  text: '2022',
                 ),
               ],
-            )
-          ],
+            ),
+          ),
+          body: TabBarView(
+            children: containers,
+          ),
         ),
       ),
     );
