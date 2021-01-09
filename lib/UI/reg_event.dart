26eb1d07b3a4bac1da5eaa345edf4d3ae52a007a
tharu-98 <64526055+tharu-98@users.noreply.github.com>
2021-01-09 11:49:21 +0530

    Update reg_event.dart

--- lib/UI/reg_event.dart
+++ lib/UI/reg_event.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:nice_button/nice_button.dart';
 
 class Register extends StatefulWidget {
   Register() : super();
