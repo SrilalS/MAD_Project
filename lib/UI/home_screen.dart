44c216e366f825da1aa249315bd9a9ea7711a32e
tharu-98 <64526055+tharu-98@users.noreply.github.com>
2021-01-13 08:52:29 +0530

    club view

---
+++ lib/UI/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:mad_project/models/post_model.dart';
+
+import 'package:mad_project/services/database_service.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:mad_project/config/palette.dart';
+import 'package:mad_project/widgets/post_container.dart';
+//import 'package:mad_project/data/data.dart';
+
+//import 'package:mad_project/widgets/widgets.dart';
+//import 'package:mad_project/models/models.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          brightness: Brightness.light,
+          backgroundColor: Colors.white,
+          title: Text(
+            'C Plus',
+            style: const TextStyle(
+              color: Palette.nsbmgreen,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -1.2,
+            ),
+          ),
+          centerTitle: false,
+        ),
+        body: StreamBuilder<List<Post>>(
+          stream: DatabaseService().getPostfromPostCollection(),
+          builder: (context, snapshot) {
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return PostContainer(
+                    captiion: snapshot.data[index].caption,
+                    imageUrl: snapshot.data[index].imageUrl,
+                    clubImage: snapshot.data[index].clubImage,
+                    clubName: snapshot.data[index].clubName,
+                  );
+                });
+          },
+        )
+//        SliverToBoxAdapter(
+//          child:CreatePostContainer(currentUser:currentUser),
+//        ),
+//
+
+        );
+  }
+} // TODO Implement this library.
